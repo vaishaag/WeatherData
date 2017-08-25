6a8302e4d0301aa28839b1e1a2a1e68fa15e1cad
Vaishaag Subhagan <vaishaag@gmail.com>
2017-08-25 20:46:52 +0530

    Updated test cases

--- src/test/java/com/app/weather/TestSuite.java
+++ src/test/java/com/app/weather/TestSuite.java
@@ -4,9 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import com.app.weather.model.LocationTest;
-import com.app.weather.model.PositionTest;
-import com.app.weather.model.WeatherDetailsTest;
 import com.app.weather.service.DataServiceTest;
 import com.app.weather.service.LocationServiceTest;
 import com.app.weather.service.PredictorTest;
@@ -16,9 +13,6 @@ import com.app.weather.util.UtilsTest;
 @RunWith(Suite.class)
 @SuiteClasses({ 
 	ApplicationTest.class, 
-	LocationTest.class,
-	PositionTest.class,
-	WeatherDetailsTest.class,
 	DataServiceTest.class,
 	LocationServiceTest.class,
 	PredictorTest.class,
